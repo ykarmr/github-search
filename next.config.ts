419eb22533a8bf626285a07bae4b45e661914d80
ykarmr <yuki.arimura0508@gmail.com>
2025-09-05 13:13:58 +0900

    next.config.tsから画像最適化設定を削除

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // 画像最適化の設定
-  images: {
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "avatars.githubusercontent.com",
-        port: "",
-        pathname: "/u/**",
-      },
-    ],
-  },
-
   experimental: {
     optimizePackageImports: ["@tailwindcss/postcss"],
   },
